ca061e8e7d77dc836e2fd12ca36a946bce9ff94f
Mike <kenr.mk@gmail.com>
2019-05-08 15:57:43 +0200

    added empty style

--- index.esm.js
+++ index.esm.js
@@ -15,6 +15,18 @@ export var {Map} = mapboxgl
 // needed to get hold of map instance, without having to do some nasty get/set Object.defineProperty hacks.
 // Maybe will be implemented in the future. Not enough time for now.
 Map.prototype.initToolbox = function() {
+	//preloadEmptyStyle(this)
 	initTiltGesture(this)
 	//initZoomGesture(this)
+}
+
+function preloadEmptyStyle(map) {
+	var style = map.getStyle()
+	if (style) return
+	map.setStyle({
+		version: 8,
+		name: 'empty',
+		sources: {},
+		layers: []
+	})
 }
